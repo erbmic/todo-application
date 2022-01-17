24bb9bceda4847fdf3eb634540dc35dfbca4e2ff
Michael Erb <michael_erb@bluewin.ch>
2022-01-17 19:42:33 +0100

    todocontroller add delete

--- src/main/java/todo/rest/controller/TodoController.java
+++ src/main/java/todo/rest/controller/TodoController.java
@@ -31,6 +31,7 @@ public class TodoController extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String keyword = request.getParameter("keyword");
         User user = (User)request.getAttribute("user");
+
         List<ToDo> toDos = new ArrayList<>();
         if (keyword != null) {
             try {
@@ -49,21 +50,34 @@ public class TodoController extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         User user = (User)request.getAttribute("user");
-
         ToDo toDo = objectMapper.readValue(request.getReader(), ToDo.class);
 
         try {
             user.getTodoList().addTodo(toDo.getTitle(), String.valueOf(toDo.getImportant()), String.valueOf(toDo.getDueDate()), toDo.getCategory(), toDo.getDescription());
-        } catch (InvalidTodoTitleException | InvalidTodoDueDateException e) {
-            e.printStackTrace();
-        }
+            response.setStatus(HttpServletResponse.SC_CREATED);
 
-        response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+        } catch (InvalidTodoTitleException | InvalidTodoDueDateException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
     }
 
     @Override
     public void doDelete(HttpServletRequest request, HttpServletResponse response) {
+        User user = (User)request.getAttribute("user");
+        String path = request.getPathInfo();
 
+        if (path == null || path.equals("/")) {
+            response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+        } else {
+            try {
+                String id = path.substring(1);
+                user.getTodoList().deleteTodo(id);
+                response.setStatus(HttpServletResponse.SC_OK);
+//            } catch (NumberFormatException | NewsNotFoundException | NoSuchTodoIDException ex) {
+            } catch (NumberFormatException | NoSuchTodoIDException ex) {
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            }
+        }
     }
 
 }
